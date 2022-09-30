61f23e191135fca036e7bdcd4133828f758542e3
Kritsakorn Dechalert <de.kritsakorn_st@tni.ac.th>
2022-09-30 18:21:19 +0700

    first commit

---
+++ components/Styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 25,
+    alignItems: "center",
+    fontWeight: "bold",
+    color: "red",
+  },
+});
+
+export default styles;
